02b9824ae9af1e8e485e5e2f8233eac7b86a4231
mft39 <mft39anton@gmail.com>
2012-03-13 14:59:48 +0400

    translated to English votesview.php

--- to_translate/votesview.php
+++ to_translate/votesview.php
@@ -24,19 +24,19 @@ if ($_GET[requestid])
 	$perpage = 50;
 	$limit = pager($perpage, $count, array('votesview') );
 	$res = sql_query("SELECT users.id as userid,users.username, users.ratingsum, users.class, users.enabled, users.warned, users.donor requests.id as requestid, requests.request FROM addedrequests INNER JOIN users ON addedrequests.userid = users.id INNER JOIN requests ON addedrequests.requestid = requests.id WHERE addedrequests.requestid =$requestid $limit") or sqlerr(__FILE__, __LINE__);
-	$REL_TPL->stdhead("Голосовавшие");
+	$REL_TPL->stdhead("Votes");
 	$res2 = sql_query("SELECT request FROM requests WHERE id=$requestid");
 	$arr2 = mysql_fetch_assoc($res2);
 
-	print("<h1>Голосовавшие для <a href=\"".$REL_SEO->make_link('requests','id',$requestid)."\"><b>$arr2[request]</b></a></h1>");
-	print("<p>Голосовать за этот <a href=\"".$REL_SEO->make_link('requests','action','vote','voteid',$requestid)."\"><b>запрос</b></a></p>");
+	print("<h1>Voted for <a href=\"".$REL_SEO->make_link('requests','id',$requestid)."\"><b>$arr2[request]</b></a></h1>");
+	print("<p>Vote <a href=\"".$REL_SEO->make_link('requests','action','vote','voteid',$requestid)."\"><b>request</b></a></p>");
 
 	if (mysql_num_rows($res) == 0)
-	print("<p align=center><b>Ничего не найдено</b></p>\n");
+	print("<p align=center><b>Nothing there, man</b></p>\n");
 	else
 	{
 		print("<table border=1 cellspacing=0 cellpadding=5>\n");
-		print("<tr><td class=colhead>Имя</td><td class=colhead>Рейтинг</td></tr>\n");
+		print("<tr><td class=colhead>User</td><td class=colhead>Ratio</td></tr>\n");
 		while ($arr = mysql_fetch_assoc($res))
 		{
 			$ratio = ratearea($arr['ratingsum'],$arr['userid'],'users',$CURUSER['id']);
